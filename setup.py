5023080419ad9fc9e64d79f1a5d19d04a6ac524f
Colin Van Oort <cvanoort@uvm.edu>
2020-01-21 15:18:47 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup
 setup(
     name='kanerva',
     version='0.0.1',
-    install_requires=['tensorflow', 'tensorflow_probability']
+    install_requires=['tensorflow', 'tensorflow_probability'],
+    packages=['kanerva']
 )
